a689f438e9c0fac3433a025382d068d7bda6a38e
Belakhvostau Aliaksandr <aliaksandr@wsa-089-23b.local>
2022-05-06 17:37:27 +0300

    wild code

--- tree.js
+++ tree.js
@@ -1,1 +1,43 @@
+const getData = async (url) => {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
+const createTree = async () => {
+  const data = await getData("./example.json");
+  searchNames(data);
+  console.log(Object.values(data.searchProjects.items));
+};
+
+createTree();
+
+const searchNames = (data) => {
+  if (Array.isArray(data)) {
+    data.forEach((item) => {
+      const li = document.createElement("li");
+      if (item.hasOwnProperty("name")) {
+        li.textContent = item.name;
+        document.body.append(li);
+      }
+      searchNames(item);
+    });
+  } else {
+    for (let subData of Object.values(data)) {
+      if (typeof subData === "object") {
+        if (subData.hasOwnProperty("name")) {
+          const li = document.createElement("li");
+          li.textContent = subData.name;
+          document.body.append(li);
+        }
+        searchNames(subData);
+      } else {
+        continue;
+      }
+    }
+  }
+};
