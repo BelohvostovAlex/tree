5a3adc7ca12b8e7318312928bce1c631084da846
Belakhvostau Aliaksandr <aliaksandr@wsa-089-23b.local>
2022-05-10 17:18:51 +0300

    recursion

--- tree.js
+++ tree.js
@@ -1,40 +1,35 @@
-const getData = async (url) => {
-  try {
-    const response = await fetch(url);
-    const json = await response.json();
-    return json;
-  } catch (error) {
-    throw new Error(error);
-  }
-};
+document.addEventListener("DOMContentLoaded", () => {
+  const getData = async (url) => {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      return json;
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
 
-const createTree = async () => {
-  const data = await getData("./example.json");
-  searchNames(data);
-};
+  const createTree = async () => {
+    const data = await getData("./example.json");
+    objProps(data);
+  };
 
-createTree();
+  const isObject = (val) => {
+    return typeof val === "object";
+  };
 
-const searchNames = (data) => {
-  if (Array.isArray(data)) {
-    data.forEach((item) => {
-      const li = document.createElement("li");
-      if (item.hasOwnProperty("name")) {
-        li.textContent = item.name;
-        document.body.append(li);
-      }
-      searchNames(item);
-    });
-  } else {
-    for (let subData of Object.values(data)) {
-      if (typeof subData === "object") {
-        if (subData.hasOwnProperty("name")) {
+  const objProps = (obj) => {
+    for (let value of Object.values(obj)) {
+      if (isObject(value)) {
+        if (value.hasOwnProperty("name")) {
           const li = document.createElement("li");
-          li.textContent = subData.name;
+          li.textContent = value.name;
           document.body.append(li);
         }
-        searchNames(subData);
+        objProps(value);
       }
     }
-  }
-};
+  };
+
+  createTree();
+});
